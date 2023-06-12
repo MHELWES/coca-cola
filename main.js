55536b797944ede4460313f5a55890427d55a150
Mhelwes.xyz <108698438+MHELWES@users.noreply.github.com>
2023-06-12 19:35:22 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function imgSlider(zakzoki)
+{
+    document.querySelector('.cock').src = zakzoki;
+
+}
+
+function changeCircleColor (color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+    }
